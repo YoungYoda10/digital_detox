0d9a1db1f54ac9709d7e846ba58944b0bf8997a8
Vladimir Vozar <vladimir.vozar2000@gmail.com>
2023-02-10 00:57:02 +0100

    Added viewBinding in recycler adapter

--- app/src/main/java/com/dovla/infinityscroll/ui/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/dovla/infinityscroll/ui/adapter/RecyclerViewAdapter.kt
@@ -1,26 +1,23 @@
 package com.dovla.infinityscroll.ui.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.dovla.infinityscroll.R
+import com.dovla.infinityscroll.databinding.ItemRowBinding
 
-class RecyclerViewAdapter(private var mItemList: List<String?>?) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerViewAdapter(private var mItemList: List<String?>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false)
-        return ItemViewHolder(view)
+    class MyViewHolder(binding: ItemRowBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(ItemRowBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
-    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
     }
 
     override fun getItemCount(): Int {
-        return if (mItemList == null) 0 else mItemList!!.size
+        return mItemList.size
     }
-
-    private class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
